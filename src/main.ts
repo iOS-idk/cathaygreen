4fecdeca37de722c64db1ec0fd345c9a9dc9b10a
JohnKHW <wongkahingerv@gmail.com>
2022-11-20 13:40:42 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { initialize, setupSwagger } from '@util/helper';
 const { PORT } = process.env;
 
 async function bootstrap() {
+  process.setMaxListeners(0);
   const app = await NestFactory.create(AppModule);
   initialize(app);
   setupSwagger(app);
